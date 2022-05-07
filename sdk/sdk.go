b8f0bfdeae40fafeb4c935448ea0dc6d815a5482
Chris de la Iglesia <chris@cmdli.dev>
2022-05-06 17:14:34 -0700

    Refactor object read/write and add a few tests

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -7,13 +7,28 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"golang.org/x/crypto/hkdf"
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func get(url string) (string, error) {
-	response, err := http.Get("http://localhost:8080" + url)
+const BASE_URL = "http://localhost:8080"
+
+func get(path string) (string, error) {
+	response, err := http.Get(BASE_URL + path)
+	if err != nil {
+		return "", err
+	}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func post(path string, dataType string, data string) (string, error) {
+	response, err := http.Post(BASE_URL+path, dataType, strings.NewReader(data))
 	if err != nil {
 		return "", err
 	}
@@ -30,6 +45,16 @@ func hashSha256(data []byte) []byte {
 }
 
 func decryptBytes(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	decryptedData := make([]byte, len(data))
+	block.Decrypt(decryptedData, data)
+	return decryptedData, nil
+}
+
+func encryptBytes(data []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -71,11 +96,19 @@ func GetSalt(saltId string) ([]byte, error) {
 	return salt, nil
 }
 
-func GetObject(secret string, path string) (string, error) {
+func GetObjectByPath(secret string, path string) (string, error) {
 	accessKey, err := deterministicObjectAccessKey(secret, path)
 	if err != nil {
 		return "", err
 	}
+	encryptionKey, err := deterministicObjectEncryptionKey(secret, path)
+	if err != nil {
+		return "", err
+	}
+	return GetObject(accessKey, encryptionKey)
+}
+
+func GetObject(accessKey string, encryptionKey []byte) (string, error) {
 	objectBase64, err := get("/vault/object/" + accessKey)
 	if err != nil {
 		return "", err
@@ -84,15 +117,36 @@ func GetObject(secret string, path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	encryptionKey, err := deterministicObjectEncryptionKey(secret, path)
-	if err != nil {
-		return "", err
-	}
 	decryptedBytes, err := decryptBytes(encryptedBytes, encryptionKey)
 	if err != nil {
 		return "", err
 	}
 	return string(decryptedBytes), nil
+}
+
+func WriteObjectByPath(secret string, path string, data string) error {
+	accessKey, err := deterministicObjectAccessKey(secret, path)
+	if err != nil {
+		return err
+	}
+	encryptionKey, err := deterministicObjectEncryptionKey(secret, path)
+	if err != nil {
+		return err
+	}
+	return WriteObject(accessKey, encryptionKey, data)
+}
+
+func WriteObject(accessKey string, encryptionKey []byte, data string) error {
+	encryptedBytes, err := encryptBytes([]byte(data), encryptionKey)
+	if err != nil {
+		return err
+	}
+	objectBase64 := base64.URLEncoding.EncodeToString(encryptedBytes)
+	_, err = post("/vault/object/"+accessKey, "text/plain", objectBase64)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func DeriveLoginSecret(email string, password string) (string, error) {
@@ -108,5 +162,5 @@ func DeriveLoginSecret(email string, password string) (string, error) {
 }
 
 func GetMasterSecret(loginSecret string) (string, error) {
-	return GetObject(loginSecret, "/master-secret")
+	return GetObjectByPath(loginSecret, "/master-secret")
 }
--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -1,13 +1,54 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"reflect"
 	"testing"
 )
 
 func TestLoginSecret(t *testing.T) {
-	_, err := DeriveLoginSecret("email", "password")
+	loginSecret, err := DeriveLoginSecret("email", "password")
 	if err != nil {
 		t.Log("Login secret error:", err)
-		t.Fail()
+		t.FailNow()
+	}
+	if len(loginSecret) != 44 {
+		t.Log("Login secret wrong size")
+		t.FailNow()
+	}
+	_, err = base64.URLEncoding.DecodeString(loginSecret)
+	if err != nil {
+		t.Log("Login secret not base64")
+		t.FailNow()
+	}
+}
+
+func TestGetSalt(t *testing.T) {
+	data := make([]byte, 32)
+	rand.Read(data)
+	saltIdBytes := hashSha256([]byte(data))
+	saltId := base64.URLEncoding.EncodeToString(saltIdBytes)
+	salt, err := GetSalt(saltId)
+	if err != nil {
+		t.Log("Failed to get salt:", err)
+		t.FailNow()
+	}
+	if len(salt) != 32 {
+		t.Log("Invalid salt:", salt, err)
+		t.FailNow()
+	}
+	salt2, err := GetSalt(saltId)
+	if err != nil {
+		t.Log("Failed to get second salt:", err)
+		t.FailNow()
+	}
+	if len(salt2) != 32 {
+		t.Log("Invalid salt:", salt, err)
+		t.FailNow()
+	}
+	if !reflect.DeepEqual(salt, salt2) {
+		t.Log("Salt is not saved between fetches:", salt, salt2)
+		t.FailNow()
 	}
 }
