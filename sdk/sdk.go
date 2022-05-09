4a275d35094ced2e00acda28d355aadf3c3e5896
Chris de la Iglesia <chris@cmdli.dev>
2022-05-09 15:17:04 -0700

    Add vault access object, which required some refactoring of how we store objects to support AES-GCM

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -25,6 +27,11 @@ type UnsuccessfulRequestError struct {
 
 func (e *UnsuccessfulRequestError) Error() string {
 	return fmt.Sprintf("Unsuccessful request (%d): %s", e.code, e.response)
+}
+
+func isReturnCode(err error, code int) bool {
+	var requestError *UnsuccessfulRequestError
+	return errors.As(err, &requestError) && requestError.code == code
 }
 
 func get(path string) (string, error) {
@@ -59,24 +66,46 @@ func hashSha256(data []byte) []byte {
 	return bytes[:]
 }
 
-func decryptBytes(data []byte, key []byte) ([]byte, error) {
+func randomBytes(length int) ([]byte, error) {
+	bytes := make([]byte, length)
+	n, err := rand.Read(bytes)
+	if err != nil || n != length {
+		return nil, err
+	}
+	return bytes, nil
+}
+
+func decryptBytes(data []byte, key []byte, nonce []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	decryptedData := make([]byte, len(data))
-	block.Decrypt(decryptedData, data)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	decryptedData, err := gcm.Open(nil, nonce, data, nil)
+	if err != nil {
+		return nil, err
+	}
 	return decryptedData, nil
 }
 
-func encryptBytes(data []byte, key []byte) ([]byte, error) {
+func encryptBytes(data []byte, key []byte) ([]byte, []byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	encryptedData := make([]byte, len(data))
-	block.Encrypt(encryptedData, data)
-	return encryptedData, nil
+	nonce, err := randomBytes(12)
+	if err != nil {
+		return nil, nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+	encryptedData := gcm.Seal(nil, nonce, data, nil)
+	return encryptedData, nonce, nil
 }
 
 func deterministicObjectAccessKey(secret []byte, path string) (string, error) {
@@ -85,7 +114,7 @@ func deterministicObjectAccessKey(secret []byte, path string) (string, error) {
 	r := hkdf.New(sha256.New, []byte(inputData), nil, nil)
 	bytes, err := ioutil.ReadAll(io.LimitReader(r, 32))
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Could not generate access key: %w", err)
 	}
 	return base64.URLEncoding.EncodeToString(bytes), nil
 }
@@ -96,7 +125,7 @@ func deterministicObjectEncryptionKey(secret []byte, path string) ([]byte, error
 	r := hkdf.New(sha256.New, []byte(inputData), nil, nil)
 	bytes, err := ioutil.ReadAll(io.LimitReader(r, 32))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Could not generate encryption key: %w", err)
 	}
 	return bytes, nil
 }
@@ -118,6 +147,37 @@ func GetSalt(saltId string) ([]byte, error) {
 	return salt, nil
 }
 
+type EncryptedBlob struct {
+	Blob string `json:"blob"`
+	Iv   string `json:"iv"`
+}
+
+func encodeBlob(blob []byte, iv []byte) (string, error) {
+	blobBase64 := base64.URLEncoding.EncodeToString(blob)
+	ivBase64 := base64.URLEncoding.EncodeToString(iv)
+	blobJson, err := json.Marshal(&EncryptedBlob{Blob: blobBase64, Iv: ivBase64})
+	if err != nil {
+		return "", err
+	}
+	return string(blobJson), nil
+}
+
+func decodeBlob(blobJson string) ([]byte, []byte, error) {
+	var blob EncryptedBlob
+	if err := json.Unmarshal([]byte(blobJson), &blob); err != nil {
+		return nil, nil, err
+	}
+	blobBytes, err := base64.URLEncoding.DecodeString(blob.Blob)
+	if err != nil {
+		return nil, nil, err
+	}
+	ivBytes, err := base64.URLEncoding.DecodeString(blob.Iv)
+	if err != nil {
+		return nil, nil, err
+	}
+	return blobBytes, ivBytes, nil
+}
+
 func GetObjectByPath(secret []byte, path string) (string, error) {
 	accessKey, err := deterministicObjectAccessKey(secret, path)
 	if err != nil {
@@ -135,11 +195,15 @@ func GetObject(accessKey string, encryptionKey []byte) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("Could not retrieve object data: %w", err)
 	}
-	encryptedBytes, err := base64.URLEncoding.DecodeString(objectBase64)
+	blobJson, err := base64.URLEncoding.DecodeString(objectBase64)
 	if err != nil {
 		return "", fmt.Errorf("Could not decode object bytes: %w (%s)", err, objectBase64)
 	}
-	decryptedBytes, err := decryptBytes(encryptedBytes, encryptionKey)
+	blob, iv, err := decodeBlob(string(blobJson))
+	if err != nil {
+		return "", fmt.Errorf("Could not decode blob json: %w (%s)", err, blobJson)
+	}
+	decryptedBytes, err := decryptBytes(blob, encryptionKey, iv)
 	if err != nil {
 		return "", fmt.Errorf("Could not decrypt bytes: %w", err)
 	}
@@ -159,14 +223,18 @@ func WriteObjectByPath(secret []byte, path string, data string) error {
 }
 
 func WriteObject(accessKey string, encryptionKey []byte, data string) error {
-	encryptedBytes, err := encryptBytes([]byte(data), encryptionKey)
+	encryptedBytes, iv, err := encryptBytes([]byte(data), encryptionKey)
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not encrypt data: %w", err)
 	}
-	objectBase64 := base64.URLEncoding.EncodeToString(encryptedBytes)
+	blobJson, err := encodeBlob(encryptedBytes, iv)
+	if err != nil {
+		return fmt.Errorf("Could not create json: %w", err)
+	}
+	objectBase64 := base64.URLEncoding.EncodeToString([]byte(blobJson))
 	_, err = post("/vault/object/"+accessKey, "text/plain", objectBase64)
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not store encrypted data: %w", err)
 	}
 	return nil
 }
@@ -184,15 +252,10 @@ func DeriveLoginSecret(email string, password string) ([]byte, error) {
 func GetMasterSecret(loginSecret []byte) ([]byte, error) {
 	secretBase64, err := GetObjectByPath(loginSecret, "/master-secret")
 	if err != nil {
-		var requestError *UnsuccessfulRequestError
-		if !errors.As(err, &requestError) {
+		if !isReturnCode(err, 404) {
 			return nil, fmt.Errorf("Could not get master secret: %w", err)
 		}
-		if requestError.code != 404 {
-			return nil, fmt.Errorf("Could not get master secret: %w", err)
-		}
-		secretBytes := make([]byte, 32)
-		_, err = rand.Read(secretBytes)
+		secretBytes, err := NewMasterSecret()
 		if err != nil {
 			return nil, fmt.Errorf("Could not generate random bytes: %w", err)
 		}
@@ -202,5 +265,90 @@ func GetMasterSecret(loginSecret []byte) ([]byte, error) {
 		}
 		return secretBytes, nil
 	}
-	return base64.URLEncoding.DecodeString(secretBase64)
+	masterSecret, err := base64.URLEncoding.DecodeString(secretBase64)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid master secret returned from vault: %w", err)
+	}
+	return masterSecret, nil
+}
+
+func NewMasterSecret() ([]byte, error) {
+	secretBytes := make([]byte, 32)
+	n, err := rand.Read(secretBytes)
+	if err != nil || n != 32 {
+		return nil, fmt.Errorf("Could not generate random bytes: %w", err)
+	}
+	return secretBytes, nil
+}
+
+type AccessData struct {
+	accessKey     string
+	encryptionKey string
+}
+
+type KeyDirectory map[string]*AccessData
+
+func NewKeyDirectory() *KeyDirectory {
+	directory := make(KeyDirectory)
+	return &directory
+}
+
+func (directory *KeyDirectory) Load(masterSecret []byte) error {
+	keyDirectoryObject, err := GetObjectByPath(masterSecret, "/directory")
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal([]byte(keyDirectoryObject), directory); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (directory *KeyDirectory) Store(masterSecret []byte) error {
+	jsonData, err := json.Marshal(directory)
+	if err != nil {
+		return err
+	}
+	return WriteObjectByPath([]byte(jsonData), "/master-secret", string(jsonData))
+}
+
+func getKeyDirectory(masterSecret []byte) (*KeyDirectory, error) {
+	directory := NewKeyDirectory()
+	err := directory.Load(masterSecret)
+	if err != nil {
+		if !isReturnCode(err, 404) {
+			return nil, err
+		}
+		if err = directory.Store(masterSecret); err != nil {
+			return nil, err
+		}
+	}
+	return directory, nil
+}
+
+type VaultAccess struct {
+	masterSecret []byte
+	keyDirectory *KeyDirectory
+}
+
+func NewVault() *VaultAccess {
+	return &VaultAccess{masterSecret: nil, keyDirectory: nil}
+}
+
+func (access *VaultAccess) Login(email, password string) error {
+	loginSecret, err := DeriveLoginSecret(email, password)
+	if err != nil {
+		return err
+	}
+	masterSecret, err := GetMasterSecret(loginSecret)
+	if err != nil {
+		return err
+	}
+	access.masterSecret = masterSecret
+	keyDirectory, err := getKeyDirectory(access.masterSecret)
+	if err != nil {
+		return err
+	}
+	access.keyDirectory = keyDirectory
+	return nil
 }
--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -46,16 +46,47 @@ func TestGetSalt(t *testing.T) {
 func TestGetMasterSecret(t *testing.T) {
 	loginSecret, err := DeriveLoginSecret("email", "password")
 	if err != nil {
-		t.Log("Could not get login secret:", err)
+		t.Log("Could not get login secret in test:", err)
 		t.FailNow()
 	}
 	masterSecret, err := GetMasterSecret(loginSecret)
 	if err != nil {
-		t.Log("Could not get master secret:", err)
+		t.Log("Could not get master secret in test:", err)
 		t.FailNow()
 	}
 	if len(masterSecret) != 32 {
 		t.Log("Master secret invalid:", masterSecret)
+		t.FailNow()
+	}
+}
+
+func TestGetKeyDirectory(t *testing.T) {
+	masterSecret, err := NewMasterSecret()
+	if err != nil {
+		t.Logf("Error generating master secret: %s", err)
+		t.FailNow()
+	}
+	directory, err := getKeyDirectory(masterSecret)
+	if err != nil {
+		t.Logf("Error getting key directory: %s", err)
+		t.FailNow()
+	}
+	var data AccessData
+	data.accessKey = "access"
+	data.encryptionKey = "encryption"
+	(*directory)["/test"] = &data
+	err = directory.Store(masterSecret)
+	if err != nil {
+		t.Logf("Error storing directory: %s", err)
+		t.FailNow()
+	}
+}
+
+func TestVaultLogin(t *testing.T) {
+	vault := NewVault()
+	err := vault.Login("email", "password")
+	if err != nil {
+		t.Logf("Could not log into vault: %s", err)
 		t.FailNow()
 	}
 }
