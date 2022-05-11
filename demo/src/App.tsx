8da6bebc68be44376b0ddb88fc2bbc673ed06cc8
Chris de la Iglesia <chris@cmdli.dev>
2022-05-11 16:21:28 -0700

    Add Tailwind to web app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-white">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl underline">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
