6a801d6644a7eb00490a8dbd79c3fd23c7fb9e66
Chris de la Iglesia <chris@cmdli.dev>
2022-06-16 18:24:31 -0700

    Add new roles to vault_user

--- server/setup.sql
+++ server/setup.sql
@@ -2,3 +2,5 @@ create database vault;
 create role vault_user with login;
 alter user vault_user password 'insecure_password';
 GRANT ALL PRIVILEGES ON DATABASE vault TO vault_user;
+GRANT pg_read_all_data TO vault_user;
+GRANT pg_write_all_data TO vault_user;
