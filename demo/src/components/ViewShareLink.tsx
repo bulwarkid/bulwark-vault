49d95424d51708c60d543a0763aabe845f9523d9
Chris de la Iglesia <chris@cmdli.dev>
2022-06-20 19:45:11 -0700

    Refactor App location

--- demo/src/components/ViewShareLink.tsx
+++ demo/src/components/ViewShareLink.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+type ViewShareLinkProps = {
+    rawFragment: string;
+};
+
 export class ViewShareLink extends React.Component {
     render() {
         return <div />;
